e64556805bbe36953249a50f8c232c3a2c0e72b2
ShaheerHameed <115598007+ShaheerHameed@users.noreply.github.com>
2022-11-08 19:26:32 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+
+console.log(marks);
+
+for(var mark = 0; mark<=marks.length; mark++){
+  if(marks[mark]<45){
+    console.log(marks[mark]);
+  }
+}
+
+
+function setup() 
+{
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
